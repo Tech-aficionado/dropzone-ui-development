0408eca9de2fe77c4b546f8b68e029362cb47865
Shivansh Goel <shivansh.goela12@gmail.com>
2024-12-16 09:00:20 +0530

    added skeleton

---
+++ src/app/components/product-tile-view-skeleton/product-tile-view-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-tile-view-skeleton',
+  templateUrl: './product-tile-view-skeleton.component.html',
+  styleUrl: './product-tile-view-skeleton.component.css'
+})
+export class ProductTileViewSkeletonComponent {
+
+}
