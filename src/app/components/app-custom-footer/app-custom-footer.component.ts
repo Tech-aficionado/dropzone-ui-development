17a9bf506f32ca46575df03a0918de309d8b998a
Shivansh Goel <shivansh.goela12@gmail.com>
2024-12-18 12:45:11 +0530

    fixed footer

--- src/app/components/app-custom-footer/app-custom-footer.component.ts
+++ src/app/components/app-custom-footer/app-custom-footer.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-app-custom-footer',
   templateUrl: './app-custom-footer.component.html',
-  styleUrl: './app-custom-footer.component.css'
+  styleUrl: './app-custom-footer.component.css',
 })
-export class AppCustomFooterComponent {
-
-}
+export class AppCustomFooterComponent {}
