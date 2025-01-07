f6d9e0fcf7c3b67a98f87eb5236ebb17f028e997
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-07 22:03:32 +0530

    added custom footer

---
+++ src/app/components/custom-footer/custom-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-footer',
+  templateUrl: './custom-footer.component.html',
+  styleUrl: './custom-footer.component.css',
+})
+export class CustomFooterComponent {}
