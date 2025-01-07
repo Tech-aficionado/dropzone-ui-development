f730459a57067704751b65cec8603df7d8244eeb
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-07 21:58:20 +0530

    deleted custom footer

--- src/app/components/app-custom-footer/app-custom-footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-app-custom-footer',
-  templateUrl: './app-custom-footer.component.html',
-  styleUrl: './app-custom-footer.component.css',
-})
-export class AppCustomFooterComponent {}
