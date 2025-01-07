e7ed95dbdcafc68a7c9f57ca46b19621b1f03246
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-07 22:04:02 +0530

    made few changes in input component based on new form

--- src/app/components/ng-input-box/ng-input-box.component.ts
+++ src/app/components/ng-input-box/ng-input-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -18,8 +18,25 @@ export class NgInputBoxComponent {
   @Input() form!: FormGroup;
   @Input() inputToolTip!: string;
   @Input() color: string = '#919191';
+  @Input() toggleMask: boolean = false;
+  @Input() passwordtype!: boolean ;
+  @Input() HelperText!: string ;
   @Input() label_color: string = '#919191';
   @Input() input_bg: string = '#222222';
   @Input() input_color: string = '#919191';
+  @Input() errorMessage: string | undefined;
+  @Output() blur = new EventEmitter<void>();
   constructor() {}
+  onInputBlur() {
+    this.blur.emit();
+  }
+
+  shouldShowError(): boolean {
+    const control = this.form.get(this.fieldId);
+    return (control && control.invalid && (control.dirty || control.touched)) ?? false;
+  }
+  shouldShowHelperText(): boolean {
+    const control = this.form.get(this.fieldId);
+    return (control && control.valid && (control.dirty || control.touched)) ?? false;
+  }
 }
