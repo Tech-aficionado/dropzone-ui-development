9d931ab801ed5e144d93f538f87a48e262e2fc76
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-07 22:04:11 +0530

    github auth

---
+++ src/app/Elements/facebook-auth-button/facebook-auth-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-auth-button',
+  templateUrl: './facebook-auth-button.component.html',
+  styleUrl: './facebook-auth-button.component.css'
+})
+export class FacebookAuthButtonComponent {
+
+}
