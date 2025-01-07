c1968a497eb922301ed22a865556683020e59a65
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-07 22:04:27 +0530

    add grid loading component

---
+++ src/app/Elements/grid-loading/grid-loading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-loading',
+  templateUrl: './grid-loading.component.html',
+  styleUrl: './grid-loading.component.css',
+})
+export class GridLoadingComponent {}
