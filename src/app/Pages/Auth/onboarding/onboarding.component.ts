b545d7e8e17e68d22d20500cedc12fdb56cbb769
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-15 00:09:27 +0530

    added onBoarding for register

---
+++ src/app/Pages/Auth/onboarding/onboarding.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding',
+  templateUrl: './onboarding.component.html',
+  styleUrl: './onboarding.component.css',
+})
+export class OnboardingComponent {}
