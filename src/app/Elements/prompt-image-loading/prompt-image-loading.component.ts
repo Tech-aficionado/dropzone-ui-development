641c2ca30509856f869f60d768a7ed050b67bdd3
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-15 00:06:18 +0530

    prompt image loading modified

--- src/app/Elements/prompt-image-loading/prompt-image-loading.component.ts
+++ src/app/Elements/prompt-image-loading/prompt-image-loading.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-prompt-image-loading',
   templateUrl: './prompt-image-loading.component.html',
-  styleUrl: './prompt-image-loading.component.css'
+  styleUrl: './prompt-image-loading.component.css',
 })
-export class PromptImageLoadingComponent {
-
-}
+export class PromptImageLoadingComponent {}
