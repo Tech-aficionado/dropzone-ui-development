19eb93001a53c56b07c6432c8af81d46a5bd8d2b
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-15 00:08:25 +0530

    added service #1

---
+++ src/app/Pages/Services/product-track-page/product-track-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-track-page',
+  templateUrl: './product-track-page.component.html',
+  styleUrl: './product-track-page.component.css',
+})
+export class ProductTrackPageComponent {}
