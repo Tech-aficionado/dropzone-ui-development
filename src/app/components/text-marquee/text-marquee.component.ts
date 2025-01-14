af0b6d90b153010f7e4fe69087d876e83741e996
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-15 00:06:56 +0530

    add text marque

---
+++ src/app/components/text-marquee/text-marquee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-marquee',
+  templateUrl: './text-marquee.component.html',
+  styleUrl: './text-marquee.component.css'
+})
+export class TextMarqueeComponent {
+
+}
