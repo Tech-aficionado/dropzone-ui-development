cdbef0b633300546b161812620201fd301d449ee
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-15 00:06:00 +0530

    modified auth-card button

--- src/app/Elements/auth-card-button/auth-card-button.component.ts
+++ src/app/Elements/auth-card-button/auth-card-button.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-auth-card-button',
   templateUrl: './auth-card-button.component.html',
-  styleUrl: './auth-card-button.component.css'
+  styleUrl: './auth-card-button.component.css',
 })
 export class AuthCardButtonComponent {
-@Input() Title!: string
-@Input() disabled!: boolean
+  @Input() Title!: string;
+  @Input() disabled!: boolean;
 }
