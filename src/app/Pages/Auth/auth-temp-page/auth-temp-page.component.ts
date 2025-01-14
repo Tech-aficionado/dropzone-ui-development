a66dca4d30b79fa1b69e5a9ae6682e36d240eb6a
Shivansh Goel <shivansh.goela12@gmail.com>
2025-01-15 00:06:45 +0530

    auth temp

--- src/app/Pages/Auth/auth-temp-page/auth-temp-page.component.ts
+++ src/app/Pages/Auth/auth-temp-page/auth-temp-page.component.ts
@@ -1,29 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-auth-temp-page',
   templateUrl: './auth-temp-page.component.html',
-  styleUrl: './auth-temp-page.component.css'
+  styleUrl: './auth-temp-page.component.css',
 })
-export class AuthTempPageComponent implements OnInit{
-  constructor(private route: ActivatedRoute,
-      private JWTService: JwtHelperService,){}
-   ngOnInit(): void {
+export class AuthTempPageComponent implements OnInit {
+  
+  show: boolean = true;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private JWTService: JwtHelperService,
+
+  ) {}
+  ngOnInit(): void {
     const hashFragment = window.location.hash.substring(1); // Remove the # symbol
     const params = new URLSearchParams(hashFragment);
-      
-    if (window.location.href.includes('github')) this.route.queryParams
-    .subscribe(params2 => {
-      console.log(JSON.parse(params2['tokenization'])['user']); // { order: "popular" }
-    }
-  );
-      
-      const authUser = params.get('id_token');
-    
-      console.log(this.JWTService.decodeToken(authUser!))
 
-    
-   }
+    if (window.location.href.includes('github'))
+      this.route.queryParams.subscribe((params2) => {
+        console.log(JSON.parse(params2['tokenization'])['user']); // { order: "popular" }
+      });
+
+    const authUser = params.get('id_token');
+
+    console.log(this.JWTService.decodeToken(authUser!));
+
+
+      
+        this.show = this.router.url.toLowerCase().includes('google') ? true : false;
+
+  }
 }
